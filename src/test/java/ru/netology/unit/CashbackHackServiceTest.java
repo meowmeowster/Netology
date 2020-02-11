e9d61606a75feef7969299d7dec85cef23f8ac2f
Ivan Petrov <ivpetrov@fil-it.ru>
2020-02-11 23:14:49 +0300

    features + csv

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -2,54 +2,13 @@
 package ru.netology.unit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.service.CashbackHackService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CashbackHackServiceTest {
-	@Test
-	void shouldReturn0IfAmountIs2000() {
-		CashbackHackService service = new CashbackHackService();
-		int amount = 2000;
-
-		int actual = service.remain(amount);
-		int expected = 0;
-
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	void shouldReturn1IfAmountIs999() {
-		CashbackHackService service = new CashbackHackService();
-		int amount = 999;
-
-		int actual = service.remain(amount);
-		int expected = 1;
-
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	void shouldReturn999IfAmountIs1() {
-		CashbackHackService service = new CashbackHackService();
-		int amount = 1;
-
-		int actual = service.remain(amount);
-		int expected = 999;
-
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	void shouldReturn999IfAmountIs1001() {
-		CashbackHackService service = new CashbackHackService();
-		int amount = 1001;
-
-		int actual = service.remain(amount);
-		int expected = 999;
-
-		assertEquals(expected, actual);
-	}
 
 	@Test
 	void shouldReturn1IfAmountIs999999999() {
@@ -89,7 +48,7 @@ class CashbackHackServiceTest {
 	@Test
 	void shouldReturnErrorIfOverflow() {
 		CashbackHackService service = new CashbackHackService();
-		int amount = 999999999+999999999+999999999;
+		int amount = 999999999 + 999999999 + 999999999;
 		String expected = "amount must be greater than zero";
 		try {
 			service.remain(amount);
@@ -98,4 +57,13 @@ class CashbackHackServiceTest {
 		}
 	}
 
+	@ParameterizedTest
+	@CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+	void shouldReturnDataAccordingToTable(String input, String expect) {
+		CashbackHackService service = new CashbackHackService();
+		int amount = Integer.parseInt(input);
+		int expected = Integer.parseInt(expect);
+		int actual = service.remain(amount);
+		assertEquals(expected, actual);
+	}
 }
